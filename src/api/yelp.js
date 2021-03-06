9e931a385ec809998e1310d6343e06cbfacc8ea8
katie <kj0203@gmail.com>
2021-03-05 20:02:25 -0500

    updated

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,8 +4,10 @@ export default axios.create({
   baseURL: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses",
   headers: {
     'Authorization': `${process.env.REACT_APP_API_KEY}`,
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, GET , DELETE , PUT , OPTIONS',
+'Access-Control-Allow-Headers': 'Content-Type'
 
 
 }
