0b3917faadf8bd44adc72e46a4e8b46910b56442
katie <kj0203@gmail.com>
2021-03-06 10:02:06 -0500

    updated

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,12 +1,11 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses",
+  baseURL:"https://floating-brushlands-33402.herokuapp.com/https://api.yelp.com/v3/businesses",
   headers: {
     'Authorization': `${process.env.REACT_APP_API_KEY}`,
-    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Methods': 'POST, GET , DELETE , PUT , OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type'
 
