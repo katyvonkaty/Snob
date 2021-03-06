569f872f76b412027d86ee9972e1fd71f161ac05
katie <kj0203@gmail.com>
2021-03-05 20:51:49 -0500

    updated

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -6,8 +6,9 @@ export default axios.create({
     'Authorization': `${process.env.REACT_APP_API_KEY}`,
     'Content-Type': 'application/x-www-form-urlencoded',
     'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Methods': 'POST, GET , DELETE , PUT , OPTIONS',
-'Access-Control-Allow-Headers': 'Content-Type'
+    'Access-Control-Allow-Headers': 'Content-Type'
 
 
 }
